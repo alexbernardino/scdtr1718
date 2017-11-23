751dfd372d79d64999edaad63b7169945ac0bbe7
alexbernardino <alex@isr.ist.utl.pt>
2017-11-23 16:38:50 +0000

    priority test

--- threads/threadhelloprio.cpp
+++ threads/threadhelloprio.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 void concurrent_function(char ID)  {
   int counter = 0; 
-  while(++counter  < 100) {
+  while(++counter  < 1000) {
       cout << ID;
       //this_thread::yield();
    }
@@ -19,16 +19,17 @@ int main()   {
   
   thread t1 {concurrent_function,'A'};
   cout << "Created Child Thread 1 # " << t1.get_id() << endl;
-  h1 = t1.native_handle();
-  p1.sched_priority = 10;
-  pthread_setschedparam(h1, SCHED_FIFO, &p1);
-  
   
   thread t2 {concurrent_function,'B'};
   cout << "Created Child Thread 2 # "  << t2.get_id() << endl;
+  
+  h1 = t1.native_handle();
+  p1.sched_priority = 1;
+  pthread_setschedparam(h1, SCHED_RR, &p1);
+  
   h2 = t2.native_handle();
-  p2.sched_priority = 50;
-  pthread_setschedparam(h2, SCHED_FIFO, &p2);
+  p2.sched_priority = 99;
+  pthread_setschedparam(h2, SCHED_RR, &p2);
   
   t1.join(); //wait until t1 finishes
   t2.join(); //wait until t2 finishes
